d8458a5d2a9a0d323dcbeb16d0f0ca0cc9328c40
Angela Davletova <angeladavlet@gmail.com>
2017-11-23 17:19:28 +0300

    NEW: init for class_files_data

--- src/kernel/class_files_data.cpp
+++ src/kernel/class_files_data.cpp
@@ -4,8 +4,35 @@ class_files_data::class_files_data ()
 {
 }
 
-std::string class_files_data::init (std::string /*header_file_path*/)
+std::string class_files_data::init (std::string header_file_path)
 {
+  //проверка на .h
+  std::ifstream file_h (header_file_path);
+  if (!file_h)
+    return header_file_path + " does not exist\n";
+
+  int i = 0;
+  std::string s;
+  while (i < header_file_path.length ())
+    {
+      if (header_file_path[i] != '/')
+        s += header_file_path[i];
+      else
+        s.clear ();
+      i++;
+    }
+  file_h.close();
+  m_files_names.push_back (s);
+  header_file_path.pop_back ();
+  header_file_path += "cpp";
+  std::ifstream file_cpp (header_file_path);
+  if (file_cpp)
+    {
+      s.pop_back ();
+      s += "cpp";
+      m_files_names.push_back (s);
+      file_cpp.close();
+    }
   return {};
 }
 
--- src/pch/pch.h
+++ src/pch/pch.h
@@ -3,5 +3,7 @@
 #include <vector>
 #include <string>
 #include <memory>
+#include <fstream>
+#include <iostream>
 #include "src/kernel/enum_helper.h"
 #endif
